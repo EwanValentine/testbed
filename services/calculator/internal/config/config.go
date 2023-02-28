5a1ebc8866c49f96430fe10258f232641b00cf41
Ewan Valentine <ewan.valentine89@gmail.com>
2023-02-28 12:06:11 +0000

    fix: more template fixes

--- services/calculator/internal/config/config.go
+++ services/calculator/internal/config/config.go
@@ -11,10 +11,11 @@ type Config struct {
 	Addr string `env:"ADDR,default=:9000"`
 }
 
-func Load() {
+func Load() Config {
 	var c Config
 	ctx := context.Background()
 	if err := envconfig.Process(ctx, &c); err != nil {
 		log.Fatal(err)
 	}
+	return c
 }
--- templates/grpc-service/{{cookiecutter.service_name}}/internal/config/config.go
+++ templates/grpc-service/{{cookiecutter.service_name}}/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"context"
 	"log"
 
 	envconfig "github.com/sethvargo/go-envconfig"
@@ -10,10 +11,11 @@ type Config struct {
 	Addr string `env:"ADDR,default=:9000"`
 }
 
-func Load() {
+func Load() Config {
 	var c Config
 	ctx := context.Background()
 	if err := envconfig.Process(ctx, &c); err != nil {
 		log.Fatal(err)
 	}
+	return c
 }
